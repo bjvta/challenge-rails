3ab4e8fa7fca382ddc1ff94b06e3077a4d1c2325
Brandon Jason <bjvtamayo78@gmail.com>
2023-01-30 16:11:07 -0400

    Add the creation of the profiles with its validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Profile < ApplicationRecord
-  validates :username, presence: true
+  validates :username, presence: true,
+                       format: { with: /\A[a-zA-Z0-9-]*(-)?\z/,
+                                 message: 'Invalid format, it should be <username> or <username>-<word or number>' }
   validates_uniqueness_of :username
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe Profile, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:username) }
+    it { should allow_value('brandon-1').for(:username) }
+    it { should_not allow_value(',,,A').for(:username) }
     subject { Profile.new(username: 'something') }
     it { should validate_uniqueness_of(:username) }
   end
